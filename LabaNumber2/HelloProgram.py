158dac28a318e1644edea2c7e7eb38d3b0c4703f
Aleksandr787 <shuverov.aleksandr@gmail.com>
2024-12-12 01:57:40 +0300

    master: update laba 2 - added mode from file, class HelloModes, refactoring code

--- LabaNumber2/HelloProgram.py
+++ LabaNumber2/HelloProgram.py
@@ -1,35 +1,84 @@
-#!/usr/bin/python3
-
-import re
 from sys import stderr, stdin
 
 
-def name_safe_validate(name: str):
-    if len(name.strip()) == 0:
-        raise Exception("Error: name does not exist!")
-    if not name.isalpha():
-        raise Exception("Error: name must begin with a letter!")
-    if not name[0].isupper():
-        raise Exception("Error: name must begin with a capital letter!")
+class ValidateUtil:
+
+    @staticmethod
+    def name_validate(name: str) -> bool:
+        if len(name.strip()) == 0:
+            return False
+        if not name.isalpha():
+            return False
+        if not name[0].isupper():
+            return False
+
+        return True
 
 
-def hello(name_list: list[str]):
-    for name in name_list:
+class HelloModes:
+    def hello_mode_file(self, filepath: str):
+        errors = []
+
         try:
-            name_safe_validate(name)
-            print(f"Nice to see you {name}!")
+            with open(filepath, 'r') as file:
+                name_list = [line.strip() for line in file]
+
+            self.__hello_mode_file_internal(name_list, errors)
+
+            # Записываем ошибки в файл, если они есть
+            if errors:
+                with open('error.txt', 'w') as error_file:
+                    for error in errors:
+                        error_file.write(error + '\n')
+
+        except FileNotFoundError:
+            print(f"Error: The file '{filepath}' was not found.", file=stderr)
         except Exception as e:
             print(f"Error: {str(e)}", file=stderr)
 
+    def hello_mode_console(self):
+        name_list = []
+        try:
+            for line in stdin:
+                name_list.append(line.strip())
+
+            self.__hello_mode_console_internal(name_list)
+        except KeyboardInterrupt:
+            print("\nGoodbye!")
+
+    @staticmethod
+    def __hello_mode_console_internal(name_list: list[str]):
+        for name in name_list:
+            try:
+                if not ValidateUtil.name_validate(name):
+                    raise Exception("The name doesn't follow the rules")
+                print(f"Nice to see you {name}!")
+            except Exception as e:
+                print(f"Error: {str(e)}", file=stderr)
+
+    @staticmethod
+    def __hello_mode_file_internal(name_list: list[str], errors: list[str]):
+        for name in name_list:
+            try:
+                if not ValidateUtil.name_validate(name):
+                    raise Exception("The name doesn't follow the rules")
+                print(f"Nice to see you {name}!")
+            except Exception as e:
+                error_message = f"Error for '{name}': {str(e)}"
+                errors.append(error_message)
+
 
 def main():
-    name_list = []
-    try:
-        for line in stdin:
-            name_list.append(line.strip())
-        hello(name_list)
-    except KeyboardInterrupt:
-        print("\nGoodbye!")
+    mode = input("Choose mode: 'file' or 'console' ").strip().lower()
+    hello_modes = HelloModes()
+
+    if mode == "file":
+        filepath = "name_list.txt"
+        hello_modes.hello_mode_file(filepath)
+    elif mode == "console":
+        hello_modes.hello_mode_console()
+    else:
+        raise Exception("Error: Invalid mode selected.")
 
 
 if __name__ == "__main__":
